cc952a00ed0fb3d3561f8d6c498c2107fe4882cc
aaryaamoharir <66750249+aaryaamoharir@users.noreply.github.com>
2025-06-12 10:00:05 -0500

    added the initial fgsm attack

---
+++ attacks/fgsm.py
@@ -0,0 +1,299 @@
+#used an online repo and based it off of that  
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+from torch.autograd import *
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+
+from torchvision.models import resnet18, resnet34, resnet50, resnet101, resnet152
+
+import pickle
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+lr = 0.01
+print(device)
+
+print('==> Preparing data..')
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(
+    root='./data', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=128, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(
+    root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=100, shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Model
+print('==> Building model..')
+
+net = resnet18(weights=None)
+net = net.to(device)
+if device == 'cuda':
+    net = torch.nn.DataParallel(net)
+    cudnn.benchmark = True
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=lr,
+                      momentum=0.9, weight_decay=5e-4)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+
+def train(epoch, net):
+
+    '''
+    this function train net on training dataset
+    '''
+
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+    return train_loss/len(trainloader)
+
+def test(epoch, net):
+
+    '''
+    This function evaluate net on test dataset
+    '''
+
+    global acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+    acc = 100 * correct / total
+    return test_loss/len(testloader)
+
+train_losses=[]
+test_losses=[]
+epochs=40
+
+for epoch in range(0,epochs):
+    train_losses.append(train(epoch, net))
+    test_losses.append(test(epoch, net))
+    scheduler.step()
+print('Accuracy of the network on the test images: %d %%' % (acc))
+
+epochs=40
+plt.plot(np.arange(1,epochs+1),train_losses, label='train losses')
+plt.plot(np.arange(1,epochs+1), test_losses, label='test losses')
+plt.xlabel('epochs')
+plt.ylabel('losses')
+plt.legend()
+plt.show()
+
+imgloader = torch.utils.data.DataLoader(
+    testset, batch_size=100, shuffle=False, num_workers=2)
+dataiter = iter(imgloader)
+org_images, org_labels = dataiter.next()
+
+org_labels = org_labels.to(device)
+org_images = org_images.to(device)
+print(org_images.shape)
+outputs= net(org_images)
+output=outputs.to(device)
+print(outputs.shape)
+_, predicted = torch.max(outputs.data, 1)
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.figure(figsize=(20,20))
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+samples = []
+samples_labels = []
+samples_pred = []
+selected = [3,66,67,0,26,16,4,13,1,11]
+
+for i in selected:
+  samples.append(org_images[i])
+  samples_labels.append(org_labels[i])
+  samples_pred.append(outputs[i])
+samples = torch.stack(samples)
+samples_labels = torch.stack(samples_labels)
+samples_pred = torch.stack(samples_pred)
+imshow(torchvision.utils.make_grid(samples.cpu()))
+
+def FGSM(net, x, y, eps):
+        '''
+        inputs:
+            net: the network through which we pass the inputs
+            x: the original example which we aim to perturb to make an adversarial example
+            y: the true label of x
+            eps: perturbation budget
+
+        outputs:
+            x_adv : the adversarial example constructed from x
+            h_adv: output of the last softmax layer when applying net on x_adv
+            y_adv: predicted label for x_adv
+            pert: perturbation applied to x (x_adv - x)
+        '''
+
+        x_ = Variable(x.data, requires_grad=True)
+        h_ = net(x_)
+        criterion= torch.nn.CrossEntropyLoss()
+        cost = criterion(h_, y)
+        net.zero_grad()
+        cost.backward()
+
+        #perturbation
+        pert= eps*x_.grad.detach().sign()
+
+        x_adv = x_ + pert
+
+        h_adv = net(x_adv)
+        _,y_adv=torch.max(h_adv.data,1)
+        return x_adv, h_adv, y_adv, pert
+
+print()
+print('from left to right: (1/eps) perturbation, original image, adversarial example')
+print()
+for i in selected:
+    eps=1/255
+    while True:
+        x_adv, h_adv, y_adv, pert=FGSM(net, org_images[i].unsqueeze_(0),org_labels[i].unsqueeze_(0),eps)
+        if y_adv.item()==org_labels[i].item():
+            eps=eps+(1/255)
+        else:
+            break
+    print("true label:", org_labels[i].item(), "adversary label:", y_adv.item())
+    triple=[]
+    with torch.no_grad():
+        triple.append((1/eps)*pert.detach().clone().squeeze_(0))
+        triple.append(org_images[i])
+        triple.append(x_adv.detach().clone().squeeze_(0))
+        triple=torch.stack(triple)
+        grid = torchvision.utils.make_grid(triple.cpu()/2+0.5)
+        plt.figure(figsize=(10,10))
+        plt.imshow(grid.numpy().transpose((1, 2, 0)))
+        plt.axis('off')
+        plt.show()
+print('==> Building new model..')
+net_adv = ResNet18()
+net_adv = net_adv.to(device)
+if device == 'cuda':
+    net_adv = torch.nn.DataParallel(net_adv)
+    cudnn.benchmark = True
+
+criterion = nn.CrossEntropyLoss()
+optimizer_adv = optim.SGD(net_adv.parameters(), lr=lr,
+                      momentum=0.9, weight_decay=5e-4)
+scheduler_adv = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_adv, T_max=200)
+
+def train_adv(epoch, net):
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    eps=8/255
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+
+        inputs_ = Variable(inputs.data, requires_grad=True)
+        h_ = net(inputs_)
+
+        cost = criterion(h_, targets)
+
+        net.zero_grad()
+        cost.backward()
+
+        pert= eps*inputs_.grad.detach().sign()
+        x_adv = inputs_ + pert
+
+        optimizer_adv.zero_grad()
+        outputs = net(x_adv)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer_adv.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+    return train_loss/len(trainloader)
+train_losses_adv=[]
+test_losses_adv=[]
+epochs=40
+
+for epoch in range(0,epochs):
+    train_losses_adv.append(train_adv(epoch, net_adv))
+    test_losses_adv.append(test(epoch, net_adv))
+    scheduler_adv.step()
+
+print('Accuracy of the network on unperturbed test images: %d %%' % (acc))
+
+def test_adv(net, net_adv, eps):
+    accuracy=0
+    net.train()
+    net_adv.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(testloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+
+        x_adv, h_adv, y_adv, pert = FGSM (net, inputs, targets, eps)
+
+        outputs = net_adv(x_adv)
+        loss = criterion(outputs, targets)
+
+        test_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+    accuracy = 100 * correct / total
+    return accuracy
+
+for eps in [4/355, 8/255, 12/255]:
+    accuracy=test_adv(net, net_adv, eps)
+    print("epsilon:", eps, "accuracy:", accuracy)
