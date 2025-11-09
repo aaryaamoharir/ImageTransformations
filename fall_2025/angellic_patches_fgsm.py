0e203143c43f808fa095b30f3b7aa372bf075697
Aaryaa Moharir <aaryaamoharir@gmail.com>
2025-11-08 22:54:35 -0600

    normalized for resnet56

--- fall_2025/angellic_patches_fgsm.py
+++ fall_2025/angellic_patches_fgsm.py
@@ -12,8 +12,11 @@ EPSILON = 0.01  # step size for reverse FGSM
 
 # --- Data Preparation ---
 transform = transforms.Compose([
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465),
+                         (0.2023, 0.1994, 0.2010))
 ])
+
 trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True)
 testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
